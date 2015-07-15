08e639e5a891d921fed7f89c0d214d6373091d7b
Bradley Monk <brad.monk@gmail.com>
2015-07-14 21:16:39 -0700

    functionized

--- FrameOrd.m
+++ FrameOrd.m
@@ -1,5 +1,6 @@
-FrameOrd = []; % Frame order vec
-FrameOrd(1:5) = NaN; % For subjects 3 and 4, first five trials are removed
+function [FrameO] = FrameOrd()
+FrameO = []; % Frame order vec
+FrameO(1:5) = NaN; % For subjects 3 and 4, first five trials are removed
 CSm = [1 2 3 4 5 6 7 8]; % CS- codes
 CSpp = [9 10 11 12 13 14 15 16]; % CS+ paired codes
 CSpu = [17 18 19 20 21 22 23 24]; % CS+ unpaired codes
@@ -7,17 +8,19 @@ acq_trials = length(find(strcmp(trial_data.phase,'Acquisition'))); % Determine n
 
 for i = 1:acq_trials
     if trial_data.stim(i,1) == 0 % CS- trials
-        FrameOrd = [FrameOrd CSm];
+        FrameO = [FrameO CSm];
     elseif trial_data.stim(i,1) == 1 && trial_data.shock(i,1) == 1 % CS+ paired trials
-        FrameOrd = [FrameOrd CSpp];
+        FrameO = [FrameO CSpp];
     else % CS+ unpaired trials
-        FrameOrd = [FrameOrd CSpu];
+        FrameO = [FrameO CSpu];
     end
 end
  
 % Get rid of the first five trials
 for j = 1:5
-    FrameOrd(1) = [];
+    FrameO(1) = [];
 end
 
-save('FrameOrd', 'FrameOrd');
+% save('FrameO', 'FrameO');
+end
+
